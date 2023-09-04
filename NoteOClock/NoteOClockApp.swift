2df63e3f0dcc7ae54c05c6d38f58032158b0ed02
SamuelFolledo <samuelfolledo@gmail.com>
2023-09-04 12:46:19 -0700

    initial commit

---
+++ NoteOClock/NoteOClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteOClockApp.swift
+//  NoteOClock
+//
+//  Created by Samuel Folledo on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct NoteOClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
