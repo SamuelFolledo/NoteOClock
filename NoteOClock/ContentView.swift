236d91ea43c918d6db1b909774a22edba7457234
Samuel P. Folledo <samuelfolledo@gmail.com>
2023-09-10 17:39:39 -0700

    [add] add button that creates more text cards
    Adding new card ensures that it will not be on top of each other

--- NoteOClock/ContentView.swift
+++ NoteOClock/ContentView.swift
@@ -12,21 +12,44 @@ let screenHeight: CGFloat = UIScreen.main.bounds.height
 
 struct ContentView: View {
     @StateObject var cardViewModel = CardViewModel()
+    @State var addCardCounter: CGFloat = 0
 
     var body: some View {
-        ZStack {
-            ForEach(cardViewModel.cards, id: \.id) { card in
-                ResizeableCard(card: card,
-                               viewModel: cardViewModel)
+        GeometryReader { proxy in
+            ZStack {
+                ForEach(cardViewModel.cards, id: \.id) { card in
+                    ResizeableCard(card: card,
+                                   viewModel: cardViewModel)
+                }
+
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Button {
+                            let newCard = Card(type: .text, origin: CGPoint(x: proxy.size.width / 2 + addCardCounter * 10, y: proxy.size.height / 2 + addCardCounter * 10), size: CGSize(width: 100, height: 100), id: "\(cardViewModel.cards.count)")
+                            cardViewModel.cards.append(newCard)
+                            addCardCounter += 1
+                        } label: {
+                            Image(systemName: "plus.circle")
+                                .resizable()
+                                .renderingMode(.template)
+                                .foregroundColor(Color(uiColor: .label))
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                                .padding()
+                        }
+                    }
+                }
             }
-        }
-        .onAppear {
-            let fakeCard = Card(type: .text, origin: .init(x: 80, y: 90), size: .init(width: 200, height: 200), id: "1")
-            cardViewModel.cards.append(fakeCard)
-            let fakeCard2 = Card(type: .text, origin: .init(x: 150, y: 150), size: .init(width: 150, height: 150), id: "2")
-            cardViewModel.cards.append(fakeCard2)
-            let clockCard = Card(type: .clock, origin: .init(x: 100, y: 100), size: .init(width: screenWidth / 1.4, height: 250), id: "0")
-            cardViewModel.cards.append(clockCard)
+            .onAppear {
+                let fakeCard = Card(type: .text, origin: .init(x: 80, y: 90), size: .init(width: 200, height: 200), id: "1")
+                cardViewModel.cards.append(fakeCard)
+                let fakeCard2 = Card(type: .text, origin: .init(x: 150, y: 150), size: .init(width: 150, height: 150), id: "2")
+                cardViewModel.cards.append(fakeCard2)
+                let clockCard = Card(type: .clock, origin: .init(x: 100, y: 100), size: .init(width: screenWidth / 1.4, height: 250), id: "0")
+                cardViewModel.cards.append(clockCard)
+            }
         }
     }
 }
